6d1f970c05a487e618cab846970991498d54d312
Lee.Such <lee.such@sbcglobal.net>
2013-02-19 20:48:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McommApp::Application.config.secret_token = '22d526d9355b3ce4b519f028b0ef068d57ae0563eef6600851b9033f1cf0e288f6ca65408eb523aaeae62a0ff5f60f73f42242997920a22d8dc62bfb0f10acc6'
